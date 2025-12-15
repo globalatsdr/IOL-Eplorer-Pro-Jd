53ad073c0caf235db965d876d4f48d8940ce3d70
globalatsdr <globalats.dr@gmail.com>
2025-12-15 12:22:59 +0100

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // This is crucial for GitHub Pages to find the assets
-  base: './', 
-})
