48be89db5ec9eb94966186b2a3c6969c0c430280
globalatsdr <globalats.dr@gmail.com>
2026-02-19 13:32:40 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,15 @@ export default defineConfig({
   plugins: [react()],
   // This is crucial for GitHub Pages to find the assets
   base: './', 
+  build: {
+    rollupOptions: {
+      external: ['html2canvas', 'jspdf'],
+      output: {
+        globals: {
+          html2canvas: 'html2canvas',
+          jspdf: 'jspdf'
+        }
+      }
+    }
+  }
 })
