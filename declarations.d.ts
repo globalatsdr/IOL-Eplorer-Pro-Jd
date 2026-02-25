e3ccf98e1a6b8770e4a83b585799de14dbf5c1ee
globalatsdr <globalats.dr@gmail.com>
2026-02-25 23:23:17 +0100

    Add files via upload

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,2 +1,3 @@
 declare module 'html2canvas';
 declare module 'jspdf';
+
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This is crucial for GitHub Pages to find the assets
-  base: '/', 
+ 
   build: {
     rollupOptions: {
       external: ['html2canvas', 'jspdf'],
